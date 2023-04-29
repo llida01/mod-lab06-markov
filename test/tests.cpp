4610ba70ab642aa281d27420c85193d8df9079f2
llida01 <126851615+llida01@users.noreply.github.com>
2023-04-29 19:38:12 +0300

    Add files via upload

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,12 +1,13 @@
 // Copyright 2021 GHA Test Team
 #include "gtest/gtest.h"
 #include <iostream>
+#include <fstream>
 #include <deque>
 #include <string>
 #include <map>
 #include <vector>
-#include <fstream>
 #include "textgen.h"
+typedef std::deque<std::string> prefix;
 
 TEST(test1, CountPrefix) {
     std::vector <std::string> text = ReadFile("text2.txt");
