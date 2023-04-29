5710cd393d7e77b06fb8791c561f8eb2767c9094
llida01 <126851615+llida01@users.noreply.github.com>
2023-04-29 19:28:04 +0300

    Add files via upload

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,30 +5,41 @@
 TEST(test1, CountPrefix) {
     std::vector <std::string> text = ReadFile("text2.txt");
     std::deque <prefix> prefixTable = CreatePrefixTable(text);
-    EXPECT_EQ(prefixTable[0].size(), 2);
+    bool answer = true;
+    for (int i = 0; i < prefixTable.size(); i++) {
+        if (prefixTable[i].size() != 2) {
+            answer = false;
+            break;
+        }
+    }
+    EXPECT_EQ(answer, true);
 }
 
 TEST(test2, PrefixSuffix) {
-	std::vector <std::string> text = ReadFile("text.txt");
-	std::deque <prefix> prefixTable = CreatePrefixTable(text);
-	std::map<prefix, std::vector<std::string>> statetab = CreatePrefixSuffixTable(prefixTable, text);
-	prefix pref1 = { "Заметался", "пожар" };
-	std::vector <std::string> expSuffix1 = { "голубой," };
-	prefix pref2 = { "первый", "раз" };
-	std::vector <std::string> expSuffix2 = { "я", "отрекаюсь", "я", "отрекаюсь" };
-	std::vector <std::string> suffix1 = statetab.find(pref1)->second;
-	std::vector <std::string> suffix2 = statetab.find(pref2)->second;
-	EXPECT_EQ(expSuffix1, suffix1);
-	EXPECT_EQ(expSuffix2, suffix2);
+    std::vector <std::string> text = ReadFile("text.txt");
+    std::deque <prefix> prefixTable = CreatePrefixTable(text);
+    std::map<prefix, std::vector<std::string>> statetab =
+        CreatePrefixSuffixTable(prefixTable, text);
+    prefix pref1 = { "Tips", "of" };
+    std::vector <std::string> expSuffix1 = { "blue" };
+    prefix pref2 = { "first", "time" };
+    std::vector <std::string> expSuffix2 = { "that", "refusing",
+        "that", "refusing" };
+    std::vector <std::string> suffix1 = statetab.find(pref1)->second;
+    std::vector <std::string> suffix2 = statetab.find(pref2)->second;
+    EXPECT_EQ(expSuffix1, suffix1);
+    EXPECT_EQ(expSuffix2, suffix2);
 }
 
 TEST(test3, SuffixFromOne) {
     std::vector <std::string> text = ReadFile("text1.txt");
     std::deque <prefix> prefixTable = CreatePrefixTable(text);
-    std::map<prefix, std::vector<std::string>> statetab = CreatePrefixSuffixTable(prefixTable, text);
-    prefix pref = { "В", "белом" };
-    std::string expSuffix = "плаще";
-    std::map<prefix, std::vector<std::string>>::iterator it = statetab.find(pref);
+    std::map<prefix, std::vector<std::string>> statetab =
+        CreatePrefixSuffixTable(prefixTable, text);
+    prefix pref = { "a", "white" };
+    std::string expSuffix = "cloak";
+    std::map<prefix, std::vector<std::string>>::iterator it =
+        statetab.find(pref);
     std::vector <std::string> suffix = it->second;
     int index = ChooseSuffix(suffix);
     EXPECT_EQ(expSuffix, suffix[index]);
@@ -37,10 +48,13 @@ TEST(test3, SuffixFromOne) {
 TEST(test4, SuffixFromMuch) {
     std::vector <std::string> text = ReadFile("text2.txt");
     std::deque <prefix> prefixTable = CreatePrefixTable(text);
-    std::map<prefix, std::vector<std::string>> statetab = CreatePrefixSuffixTable(prefixTable, text);
-    prefix pref = { "Имя", "твое-" };
-    std::vector <std::string> expSuffix = {"птица", "льдинка", "пять", "ах,", "поцелуй", "поцелуй" };
-    std::map<prefix, std::vector<std::string>>::iterator it = statetab.find(pref);
+    std::map<prefix, std::vector<std::string>> statetab =
+        CreatePrefixSuffixTable(prefixTable, text);
+    prefix pref = { "is", "a" };
+    std::vector <std::string> expSuffix = {"bird", "stone", "bullet,",
+        "light", "holy" };
+    std::map<prefix, std::vector<std::string>>::iterator it =
+        statetab.find(pref);
     std::vector <std::string> suffix = it->second;
     int index = ChooseSuffix(suffix);
     bool answer = false;
