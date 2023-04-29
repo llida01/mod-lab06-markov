ad2fc4a8a27d16f026b70a9e8d63e850da426c00
llida01 <126851615+llida01@users.noreply.github.com>
2023-04-29 18:30:18 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -1,5 +1,7 @@
+// Copyright 2022 UNN-IASR
 #pragma once
 #include <deque>
+#include <map>
 #include <string>
 #include <vector>
 #include <fstream>
@@ -8,9 +10,11 @@ typedef std::deque<std::string> prefix;
 
 std::vector <std::string> ReadFile(std::string name);
 bool find(std::deque <prefix> prefixTable, prefix curr);
-std::vector <std::string> CreateSuffixTable(prefix Prefix, std::vector <std::string> text);
+std::vector <std::string> CreateSuffixTable(prefix Prefix,
+  std::vector <std::string> text);
 std::deque <prefix> CreatePrefixTable(std::vector <std::string> text);
-std::map<prefix, std::vector<std::string>> CreatePrefixSuffixTable(std::deque <prefix> prefixTable, std::vector <std::string> text);
+std::map<prefix, std::vector<std::string>> CreatePrefixSuffixTable(
+  std::deque <prefix> prefixTable, std::vector <std::string> text);
 int ChooseSuffix(std::vector<std::string> suffix);
 std::string Generation(std::deque <prefix> prefixTable, int length);
 int CountWords(std::string text);
