a8c0b2cc29e496d09e1ee08719924276148b655f
llida01 <126851615+llida01@users.noreply.github.com>
2023-04-29 18:07:36 +0300

    Add files via upload

---
+++ include/textgen.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <deque>
+#include <string>
+#include <vector>
+#include <fstream>
+
+typedef std::deque<std::string> prefix;
+
+std::vector <std::string> ReadFile(std::string name);
+bool find(std::deque <prefix> prefixTable, prefix curr);
+std::vector <std::string> CreateSuffixTable(prefix Prefix, std::vector <std::string> text);
+std::deque <prefix> CreatePrefixTable(std::vector <std::string> text);
+std::map<prefix, std::vector<std::string>> CreatePrefixSuffixTable(std::deque <prefix> prefixTable, std::vector <std::string> text);
+int ChooseSuffix(std::vector<std::string> suffix);
+std::string Generation(std::deque <prefix> prefixTable, int length);
+int CountWords(std::string text);
