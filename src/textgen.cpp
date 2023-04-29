de538f547f763b77635e2510dee3c1dfb72290fa
llida01 <126851615+llida01@users.noreply.github.com>
2023-04-29 18:47:44 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -1,3 +1,4 @@
+// Copyright 2022 UNN-IASR
 #include <iostream>
 #include <deque>
 #include <string>
@@ -37,21 +38,18 @@ std::vector <std::string> ReadFile(std::string name) {
     return text;
 }
 
-bool find(std::deque <prefix> prefixTable, prefix curr)
-{
+bool find(std::deque <prefix> prefixTable, prefix curr) {
     std::deque<prefix>::iterator itr;
     itr = find(prefixTable.begin(), prefixTable.end(), curr);
-    if (itr != prefixTable.end())
-    {
+    if (itr != prefixTable.end()) {
         return true;
-    }
-    else
-    {
+    } else {
         return false;
     }
 }
 
-std::vector <std::string> CreateSuffixTable(prefix Prefix, std::vector <std::string> text) {
+std::vector <std::string> CreateSuffixTable(prefix Prefix,
+  std::vector <std::string> text) {
     std::vector <std::string> suffix;
     for (int i = 0; i < text.size() - NPREF; i++) {
         for (int j = 0; j < NPREF; j++) {
@@ -80,9 +78,11 @@ std::deque <prefix> CreatePrefixTable(std::vector <std::string> text) {
     return prefixTable;
 }
 
-std::map<prefix, std::vector<std::string>> CreatePrefixSuffixTable(std::deque <prefix> prefixTable, std::vector <std::string> text) {
+std::map<prefix, std::vector<std::string>> CreatePrefixSuffixTable(
+  std::deque <prefix> prefixTable, std::vector <std::string> text) {
     for (int i = 0; i < prefixTable.size(); i++) {
-        std::vector <std::string> suffix = CreateSuffixTable(prefixTable[i], text);
+        std::vector <std::string> suffix =
+            CreateSuffixTable(prefixTable[i], text);
         statetab.insert(make_pair(prefixTable[i], suffix));
     }
     return statetab;
@@ -106,7 +106,8 @@ std::string Generation(std::deque <prefix> prefixTable, int length) {
     for (int i = 0; i < nextPrefix.size(); i++) {
         genText = genText + nextPrefix[i] + " ";
     }
-    std::map<prefix, std::vector<std::string>>::iterator it = statetab.find(nextPrefix);
+    std::map<prefix, std::vector<std::string>>::iterator it =
+        statetab.find(nextPrefix);
     for (int i = 0; i < length - NPREF; i++) {
         if (it != statetab.end()) {
             std::vector <std::string> suffix = it->second;
@@ -119,8 +120,7 @@ std::string Generation(std::deque <prefix> prefixTable, int length) {
                 it->second.erase(iter + index);
                 it = statetab.find(nextPrefix);
             }
-        }
-        else {
+        } else {
             break;
         }
     }
